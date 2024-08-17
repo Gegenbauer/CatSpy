7993d9099c8b4787cd3edc30bccc39bfae9cf07d
Gegenbauer <865815634@qq.com>
2024-08-17 16:29:51 +0800

    Reduce memory usage when truncating content.

--- javaext/src/main/kotlin/me/gegenbauer/catspy/java/ext/Strings.kt
+++ javaext/src/main/kotlin/me/gegenbauer/catspy/java/ext/Strings.kt
@@ -2,11 +2,16 @@ package me.gegenbauer.catspy.java.ext
 
 fun String.capitalize() = this.replaceFirstChar { it.uppercase() }
 
+private val stringBuilder = ThreadLocal.withInitial { StringBuilder() }
+
 fun String.maxLength(maxLength: Int, ellipsisEndOrStart: Boolean = true, ellipsis: String = "..."): String {
     if (this.length <= maxLength) return this
-    return if (ellipsisEndOrStart) {
-        this.substring(0, maxLength - ellipsis.length) + ellipsis
+    val result = stringBuilder.get()
+    result.setLength(0) // Reset the StringBuilder
+    if (ellipsisEndOrStart) {
+        result.append(this, 0, maxLength - ellipsis.length).append(ellipsis)
     } else {
-        ellipsis + this.substring(this.length - maxLength + ellipsis.length)
+        result.append(ellipsis).append(this, this.length - maxLength + ellipsis.length, this.length)
     }
+    return result.toString()
 }
